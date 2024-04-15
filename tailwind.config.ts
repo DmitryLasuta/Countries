0346b4a9aeac4f8ce2041ee2654a1294de280182
DmitryLasuta <lasutadima01@gmail.com>
2024-04-16 02:16:13 +0300

    style: added color and container configurations to tailwind.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'dark-blue': 'hsl(209, 23%, 22%)', // dark mode elements
+        'very-dark-blue': 'hsl(207, 26%, 17%)', // dark mode background
+        'very-dark-black-blue': 'hsl(200, 15%, 8%)', // light mode text
+        'dark-gray': 'hsl(0, 0%, 52%)', // light mode input
+        'very-light-gray': 'hsl(0, 0%, 98%)', // light mode background
+        white: 'hsl(0, 0%, 100%)', // light mode elements
+      },
+
+      container: {
+        center: true,
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1440px',
+        },
       },
     },
   },
