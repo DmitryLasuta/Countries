c8054ae11c43d8ad248f140e59beb6044cbb262e
DmitryLasuta <lasutadima01@gmail.com>
2024-04-22 23:29:06 +0300

    fix: changed request params

--- src/api/countries/countries.api.ts
+++ src/api/countries/countries.api.ts
@@ -5,7 +5,7 @@ class CountriesApi {
   private baseUrl = 'https://restcountries.com/v3.1';
 
   public getAllShortCountryProfiles = async () => {
-    return await fetcher<ShortCountryProfile[]>(`${this.baseUrl}/all`);
+    return await fetcher<ShortCountryProfile[]>(`${this.baseUrl}/all?fields=name,flags,population,region,capital`);
   };
 }
 
