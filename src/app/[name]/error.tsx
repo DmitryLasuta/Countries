61f8c928ace19b05233685db0e1c9e47d0a3cbce
DmitryLasuta <lasutadima01@gmail.com>
2024-04-26 00:20:46 +0300

    style: added styles

--- src/app/[name]/error.tsx
+++ src/app/[name]/error.tsx
@@ -3,8 +3,11 @@
 export default function ErrorPage() {
   return (
     <div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <h1 className="text-3xl font-bold">Something went wrong. We could not find that country.</h1>
-      <p className="text-xl">Please try again or go back to the homepage.</p>
+      <h1 className="text-xl sm:text-3xl font-bold mb-4">
+        Something went wrong.
+        <br /> We could not find that country.
+      </h1>
+      <p className="text-lg sm:text-xl">Please try again or go back to the homepage.</p>
     </div>
   );
 }
