8a4b4c4df1256f297e615727827bd2f9e4bac8c2
DmitryLasuta <lasutadima01@gmail.com>
2024-04-25 23:09:10 +0300

    style: added styles for the light theme

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -6,7 +6,7 @@ const GoBackButton = () => {
   const router = useRouter();
   return (
     <button
-      className="rounded bg-slate-700 hover:bg-slate-500 text-white font-bold py-2 px-4 transition-colors"
+      className="rounded bg-slate-400 hover:bg-slate-600 text-dark-blue hover:text-white dark:bg-slate-700 dark:hover:bg-slate-500 dark:text-white font-bold py-2 px-4 transition-colors"
       type="button"
       onClick={() => router.back()}
     >
