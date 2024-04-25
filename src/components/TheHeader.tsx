7f1d67f61598edcd244c2460a9d9beeacf38d7c7
DmitryLasuta <lasutadima01@gmail.com>
2024-04-25 17:53:15 +0300

    chore: changed header level

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,6 +1,6 @@
 const TheHeader = () => {
   return (
-    <header className="bg-white p-4 dark:bg-dark-blue dark:text-white shadow">
+    <header className="bg-white p-4 dark:bg-dark-blue dark:text-white shadow border border-b-dark-blue dark:border-none">
       <div className="container">
         <h2 className="text-3xl font-bold capitalize">where in the world?</h2>
       </div>
