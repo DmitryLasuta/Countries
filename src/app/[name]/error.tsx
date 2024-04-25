72f81a8c637843827edefd5a3c949da03ce5958e
DmitryLasuta <lasutadima01@gmail.com>
2024-04-26 00:13:41 +0300

    feat: added an error page

---
+++ src/app/[name]/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function ErrorPage() {
+  return (
+    <div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <h1 className="text-3xl font-bold">Something went wrong. We could not find that country.</h1>
+      <p className="text-xl">Please try again or go back to the homepage.</p>
+    </div>
+  );
+}
